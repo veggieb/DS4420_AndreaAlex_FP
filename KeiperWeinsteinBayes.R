f25573ac0c0db33615282ad6d6926213dd90f006
veggieb <arkeiper1231@gmail.com>
2026-04-08 23:36:27 -0400

    MLP and R script

---
+++ KeiperWeinsteinBayes.R
@@ -0,0 +1,16 @@
+## Andrea Keiper and Alex Weinstein
+## DS4420 Final Project
+## April 16, 2026
+
+## MODEL 2: BAYESION ML
+
+crashes <- read.csv('Boston_CrashDetails.csv', skip=8)
+head(crashes)
+
+## Clean data
+
+
+## Figure out the distribution 
+
+
+## use brm and sampling to complete the model, categorical predicting the severity? 
